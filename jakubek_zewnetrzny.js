25613f6ee51a0ad8a0b3240ee88fe64972c6e5f0
Tost0 <144836619+Tost0@users.noreply.github.com>
2024-11-14 21:31:44 +0100

    Update and rename jakubek_zewnetrzny.js to oscik_zewnetrzny.js

--- jakubek_zewnetrzny.js
+++
@@ -1,2 +0,0 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#FF00FF").bold().fontsize(7)+"<br>"); 
-document.write(document.lastModified.strike());  
---
+++ oscik_zewnetrzny.js
@@ -0,0 +1,2 @@
+document.write("Ostatnia modyfikacja strony: ".fontcolor("#008000").bold().fontsize(6) + "<br><s>");
+document.write(document.lastModified);
